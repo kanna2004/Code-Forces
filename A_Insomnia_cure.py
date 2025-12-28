8b5253d2219e20a33dc8af8faad32b930f21f156
kanna2004 <koushiksreevathsa@gmail.com>
2025-12-28 18:02:31 +0530

    CF 148A

---
+++ A_Insomnia_cure.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+def solve():
+    k = int(input())
+    l = int(input())
+    m = int(input())
+    n = int(input())
+    d = int(input())
+    dragons = [False] * (d + 1)
+    for i in range(1, d + 1):
+        if i % k == 0 or i % l == 0 or i % m == 0 or i % n == 0:
+            dragons[i] = True
+    res = sum(1 for dragon in dragons if dragon == True )
+    print(res)
+
+if __name__ == "__main__":
+    solve()
